6776d3f71851c527717343fb255a431702a54a3f
Дарья Буртовая <gottokinbox@gmail.com>
2021-01-22 19:57:06 +0300

    кроссбраузерный запуск тестов, методы для запуска и остановки драйвера

--- src/test/java/EpamTests.java
+++ src/test/java/EpamTests.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class EpamTests {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.gen5.api.AfterAll;
+import org.junit.gen5.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+
+public class EpamTests {
+    protected static WebDriver driver;
+
+    private Logger logger = LogManager.getLogger(EpamTests.class);
+
+    @BeforeAll
+    public void setUp()
+    {
+        String opt = "headless";
+        //opt = System.getProperty("option").trim().toLowerCase();
+        driver = WebDriverFactory.create("chrome", null);
+        //WebDriverFactory.create(System.getProperty("browser").trim().toLowerCase(), opt);
+        driver.manage().window().maximize();
+        logger.info("driver initialized");
+
+    }
+
+
+    @AfterAll
+    public void close()
+    {
+        if (driver != null) driver.quit();
+        logger.info("close driver");
+    }
 }
